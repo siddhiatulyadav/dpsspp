f0892f9eff2487e8f3e927681457eb23febb4866
siddhiatulyadav <modmenugame.in@gmail.com>
2025-05-03 23:16:15 +0530

    Update start.py

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -58,9 +58,9 @@ async def start_command(client: Client, message: Message):
             force_sub_text = "**⚠️ You must join our channel(s) to use this bot!**\n\n"
             
             if config.FORCE_SUB_CHANNEL != 0:
-                force_sub_text += "• Join Channel 1\n"
+                force_sub_text += "• Join DPMods YT\n"
             if config.FORCE_SUB_CHANNEL_2 != 0:
-                force_sub_text += "• Join Channel 2\n"
+                force_sub_text += "• Join Anime Download\n"
                 
             force_sub_text += "\nJoin the channel(s) and try again."
             
